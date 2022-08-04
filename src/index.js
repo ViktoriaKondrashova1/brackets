cf58f6d60f34aff3de537d29b9f913b01d6f4499
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-08-04 15:38:32 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let open_brackets = [];
+    let brackets_pair = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let key = bracketsConfig[i][1];
+      let value = bracketsConfig[i][0];
+      open_brackets.push(bracketsConfig[i][0]);
+      brackets_pair[key] = value;
+    }
+  
+      let stack = [];
+      
+      for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+        
+          let topElement = stack[stack.length - 1];
+  
+          if (brackets_pair[currentSymbol] === topElement && stack.length > 0) {
+            stack.pop();
+          } else {
+            stack.push(currentSymbol);
+          }
+        }
+
+
+      console.log(stack)
+  
+      return stack.length === 0;
+  
+  }
